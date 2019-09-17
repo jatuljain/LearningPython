d06b1cfcb442011a228637d300a95c2a3a852aa6
JAtul <jatuljain@gmail.com>
2019-09-17 21:37:56 +0530

    adding from pycharm

--- readjson.py
+++ readjson.py
@@ -1,9 +1,20 @@
 import json
 
-fh = open("E:\\Python\\git_projects\\svt-mmp.json")
-content = fh.read()
 
-jcontent = json.loads(content)
+filename = "E:\\Python\\git_projects\\svt-mmp.json"
 
-# print("Complete json file is {} \n\n". format(jcontent))
-print(jcontent["Region"][0]["name"])
+def readjson(file):
+    fh = open(file)
+    content = fh.read()
+
+    jcontent = json.loads(content)
+
+    # print("Complete json file is {} \n\n". format(jcontent))
+    print(jcontent["Region"][0]["name"]) ## This is how we can read multi layered content of Json
+    print(jcontent["Konfiguration"][0]["infrastructure"]) ## This is how we can read multi layered content of Json
+    print(jcontent["VmwareVm"][1]["vm_image_id"]) ## This is how we can read multi layered content of Json
+
+
+
+if __file__ == "__main__":
+    readjson(filename)
