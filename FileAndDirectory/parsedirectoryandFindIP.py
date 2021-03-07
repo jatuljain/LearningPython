6d95b6c0dd0a4d674ea0df4d2d15c863a3b80ca1
Atul Jain <jatuljain@gmail.com>
2021-03-07 23:13:13 +0100

    Updated the parsing and Folder location

--- FileAndDirectory/parsedirectoryandFindIP.py
+++ FileAndDirectory/parsedirectoryandFindIP.py
@@ -1,27 +1,26 @@
 import os
-'''
+
+"""
 This program is to search particular error lines in all the files on a given directory and subdirectories
 
-'''
+"""
 
-path = 'E:\\Python\\'
+path = "D:\\Personal\\Automation\\Python\\LearningPython\\LearningPython"
 
 files = []
-# r=root, d=directories, f = files
+# r = root, d = directories, f = files
 for r, d, f in os.walk(path):
-    print("root, directory and file", r, d, f)
-    # for file in f:
-        # if '.txt' in file:
-            # files.append(os.path.join(r, file))
+    # print("root, directory and file", r, d, f)
+    for file in f:
+        if ".txt" in file:
+            files.append(os.path.join(r, file))
 
-# count = 0
-# for f in files:
-#     # print(f)
-#     with open(f) as myFile:
-#         for num, line in enumerate(myFile, 1):
-#             if "error" in line or "fail" in line:
-#                 count += 1
-#                 print(line)
-#                 print('{} found at line {} in file {}'.format(count, num, myFile) )
-
-
+count = 0
+for f in files:
+    # print(f)
+    with open(f) as myFile:
+        for num, line in enumerate(myFile, 1):
+            if "error" in line or "fail" in line:
+                count += 1
+                print("\n", line)
+                print("{} found at line {} in file {}\n\n".format(count, num, myFile))
