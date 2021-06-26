7a371d81a33da67eeb3ef8b3e870a6e269827c1f
Atul Jain <jatuljain@gmail.com>
2021-06-26 13:48:52 +0200

    Updated how to read a file from current location

--- Countcharacter.py
+++ Countcharacter.py
@@ -1,17 +1,22 @@
 import re
-para ='''asdadadasd
-    adasdad234234
-    35
-    454
-    6@%#%^$%^
-    #%#%
-    36456$%^$%6
-    35@#%$%4
-    636#$^#SDFDAFHJHsv
-    sSDFSDHD
-    yegsv%$%^$UDFVSY$a%A&%^JSRFW$^U%Ha'''
+import os
 
-fh = open('E:\\Automation\\WinFTP\\test.txt')   #### If wanna calculate the character from file
+# para = """asdadadasd
+#     adasdad234234
+#     35
+#     454
+#     6@%#%^$%^
+#     #%#%
+#     36456$%^$%6
+#     35@#%$%4
+#     636#$^#SDFDAFHJHsv
+#     sSDFSDHD
+#     yegsv%$%^$UDFVSY$a%A&%^JSRFW$^U%Ha"""
+__location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+fh = open(
+    os.path.join(__location__, "test.txt")
+)  # If wanna calculate the character from file
+
 para = fh.read()
 
 print(para)
@@ -22,14 +27,14 @@ countd = 0
 print(para)
 
 for i in para:
-    if re.match(r'[a-z]', i):
-        counts = counts+1
-    if re.match(r'[A-Z]', i):
-        countc = countc+1
-    if re.match(r'[0-9]', i):
-        countd = countd+1
+    if re.match(r"[a-z]", i):
+        counts = counts + 1
+    if re.match(r"[A-Z]", i):
+        countc = countc + 1
+    if re.match(r"[0-9]", i):
+        countd = countd + 1
 
 
-print(countc , "Total capital character")
-print(countd , "Total Digit")
+print(countc, "Total capital character")
+print(countd, "Total Digit")
 print(counts, "Total Small Character")
